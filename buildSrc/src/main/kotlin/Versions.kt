44a71f600f7f8a7c0d0f4be3d25265b3d936e682
Martin Sumera <sumera.martin@gmail.com>
2022-05-17 15:41:41 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,53 +3,53 @@
 object Versions {
 
     // gradle
-    const val androidGradlePlugin = "7.0.2"
+    const val androidGradlePlugin = "7.2.0"
 
     // plugins
-    const val detekt = "1.14.2"
-    const val ktlintGradle = "10.2.0"
+    const val detekt = "1.20.0"
+    const val ktlintGradle = "10.3.0"
     const val ktlint = "0.42.1"
 
     // kotlin
-    const val kotlin = "1.5.30"
+    const val kotlin = "1.6.21"
 
-    const val composeVersion = "1.0.3"
+    const val composeVersion = "1.2.0-beta01"
 
     // support
-    const val androidx = "1.6.0"
-    const val appcompat = "1.3.1"
-    const val lifecycle = "2.3.1"
-    const val constraintLayout = "1.0.0-alpha08"
-    const val vectorDrawable = "1.1.0"
-    const val preference = "1.1.1"
+    const val androidx = "1.9.0-alpha03"
+    const val appcompat = "1.6.0-alpha03"
+    const val lifecycle = "2.5.0-rc01"
+    const val constraintLayout = "1.0.0"
+    const val vectorDrawable = "1.2.0-beta01"
+    const val preference = "1.2.0"
 
-    const val activity = "1.3.1"
+    const val activity = "1.6.0-alpha03"
     const val desugarLibs = "1.1.5"
 
     // navigation components
-    const val navigation = "2.4.0-alpha04"
-    const val hiltNavigation = "1.0.0-alpha03"
+    const val navigation = "2.4.2"
+    const val hiltNavigation = "1.0.0"
 
     // networking
-    const val okhttp = "4.9.2"
+    const val okhttp = "4.9.3"
     const val retrofit = "2.9.0"
 
     // serialization
-    const val serializationJson = "1.3.0"
+    const val serializationJson = "1.3.3"
     const val serializationConverter = "0.8.0"
 
     // other
     const val timber = "5.0.1"
 
     // taste
-    const val arkitekt = "5.0.0"
+    const val arkitekt = "5.1.0"
 
     // hilt
-    const val hilt = "2.39.1"
+    const val hilt = "2.42"
 
     // testing
     const val testCore = "1.4.0"
     const val testRunner = "1.4.0"
-    const val junit = "1.1.3"
-    const val mockk = "1.12.0"
+    const val junit = "1.1.4-alpha06"
+    const val mockk = "1.12.4"
 }
