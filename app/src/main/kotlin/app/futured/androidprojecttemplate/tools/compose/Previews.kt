f76e72c746ad2499385bc33478f3ed537e261c06
jan.mikulik <jan.mikulik@futured.app>
2023-02-28 16:01:18 +0100

    Add more previews

--- app/src/main/kotlin/app/futured/androidprojecttemplate/tools/compose/Previews.kt
+++ app/src/main/kotlin/app/futured/androidprojecttemplate/tools/compose/Previews.kt
@@ -4,6 +4,10 @@ import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 
+private const val PHONE_XHDPI = "spec:id=reference_phone,shape=Normal,width=618,height=1098,unit=dp,dpi=320"
+private const val PHONE_XXHDPI = "spec:id=reference_phone,shape=Normal,width=412,height=732,unit=dp,dpi=480"
+private const val PHONE_XXXHDPI = "spec:id=reference_phone,shape=Normal,width=316,height=550,unit=dp,dpi=640"
+
 @Preview(
     name = "Dark mode",
     group = "UI mode",
@@ -28,14 +32,33 @@ annotation class DarkLightPreviews
 )
 annotation class FontScalePreviews
 
+@Preview(device = PHONE_XHDPI, name = "PHONE PIXEL XHDPI", group = "phoneScreenDensity")
+annotation class PhoneScreenDensityXHDPIPreview
+
+@Preview(device = PHONE_XXHDPI, name = "PHONE PIXEL XXHDPI", group = "phoneScreenDensity")
+annotation class PhoneScreenDensityXXHDPIPreview
+
+@Preview(device = PHONE_XXXHDPI, name = "PHONE PIXEL XXXHDPI", group = "phoneScreenDensity")
+annotation class PhoneScreenDensityXXXHDPIPreview
+
+@Preview(device = PHONE_XXHDPI, fontScale = 1.5f, name = "Large screen density font scale", group = "phoneScreenDensityFontScale")
 @Preview(
-    name = "Big device",
-    group = "Device sizes",
-    device = Devices.PIXEL_C
+    device = PHONE_XXXHDPI,
+    fontScale = 2.0f,
+    name = "Largest screen density font scale",
+    group = "phoneScreenDensityFontScale",
 )
-@Preview(
-    name = "Small device",
-    group = "Device sizes",
-    device = Devices.PIXEL
-)
-annotation class DeviceSizePreviews
+annotation class ScreenDensityWithFontScalePreviews
+
+@PhoneScreenDensityXHDPIPreview
+@PhoneScreenDensityXXHDPIPreview
+@PhoneScreenDensityXXXHDPIPreview
+annotation class PhoneScreenDensityPreviews
+
+@DarkLightPreviews
+@FontScalePreviews
+annotation class ComponentPreviews
+
+@DarkLightPreviews
+@ScreenDensityWithFontScalePreviews
+annotation class ScreenPreviews
